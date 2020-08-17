691225416f35dfed8bf7c9885bb571e51228dd50
Josh Machado <46457626+joshmachado@users.noreply.github.com>
2020-08-17 11:36:10 -0400

    Update ordering.py

--- scripts/ordering.py
+++ scripts/ordering.py
@@ -12,17 +12,25 @@ cat = Table.read('/Users/josh/GitHub/W51/data/coldnh3_catalog.tex')
 #Fluxes come from cat['peak']
 
 #Catalog is already sorted by fluxes, low to high
-fluxes = np.array(cat['peak'])
-fluxes = fluxes[1:len(fluxes)]
-fluxes = fluxes.astype('float64')
-
-temps = np.array(cat['KTemp'])
-temps = temps[1:len(temps)]
-temps = temps.astype('float64')
 
 masses = np.array(cat['mass'])
 masses = masses[1:len(masses)]
 masses = masses.astype('float64')
+
+fluxes = np.array(cat['peak'])
+fluxes = fluxes[1:len(fluxes)]
+fluxes = fluxes.astype('float64')
+#TRIM ZERO MASSES
+fluxes = fluxes[masses!=0]
+
+temps = np.array(cat['KTemp'])
+temps = temps[1:len(temps)]
+temps = temps.astype('float64')
+#TRIM ZERO MASSES
+temps = temps[masses!=0]
+
+#TRIM ZERO MASSES
+masses = masses[masses!=0]
 
 index = np.array(np.arange(len(fluxes)).astype('int'))
 
